c564579055ebe341cf5b560e914b4ca479281c9a
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-10-08 14:47:36 -0700

    Added SavingsAccount add_interest method

--- bank.rb
+++ bank.rb
@@ -1,2 +1,5 @@
 require "./bankaccount.rb"
 require "./savings.rb"
+
+# hey = Bank::SavingsAccount.new(1,1000000,"today")
+# hey.add_interest(0.25)
--- savings.rb
+++ savings.rb
@@ -10,15 +10,21 @@ module Bank
     def withdraw(amount)
       if amount > @balance
         puts "That's more than you have available in your account! Your account balance is $#{@balance / 100}."
-      elsif (@balance - amount) < 1000
-        puts "If you withdraw $#{amount / 100}, you will have a remaining balance below $10. You cannot have a balance of less than $10 in a savings account. Your current balance is $#{@balance / 100}."
+      elsif (@balance - amount - 200) < 1000
+        puts "If you withdraw $#{amount / 100}, you will have a remaining balance below $10. You cannot have a balance of less than $10 in a savings account. Your current balance is $#{@balance / 100} and you will be charged a transaction fee of $2 per withdrawal."
       elsif amount <= @balance
-        @balance = @balance - amount
-        puts "You have withdrawn $#{amount / 100} and your new account balance is $#{@balance / 100}."
+        @balance = @balance - amount - 200
+        puts "You have withdrawn $#{amount / 100} and your new account balance is $#{@balance / 100}. You were charged a transaction fee of $2."
       end
     end
 
     def add_interest(rate)
+      puts "The starting balance was #{@balance / 100}"
+      interest = @balance * rate/100
+      puts "The interest is $#{interest / 100}"
+      @balance += interest
+      puts "The new balance is $#{@balance / 100}"
+      return interest
     end
 
   end
